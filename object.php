c1418f1ee1e520d7cba6362957a2722a4a03ba88
sunfeilong <w_sunfeilong@xiwang.com>
2023-05-15 16:06:11 +0800

    php object

--- object.php
+++ object.php
@@ -8,9 +8,19 @@ class A {
 	function __construct(){
 		$this->{1} = 1;
 	}
+	
+	function sayHello(){
+		echo "Hello \n";
+	}
 }
 
+$a = new A();
+$a->sayHello();
 
-var_export((array) new A());
-var_dump((array) new A());
+var_export($a);
+var_dump($a);
+
+//convert to array
+var_export((array)$a);
+var_dump((array)$a);
 ?>
